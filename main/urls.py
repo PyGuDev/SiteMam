ffd67ddf6dc13cb9a92a8f830e2a99b9bbedcb48
PyGuDev <gubaev1999@gmail.com>
2020-01-23 03:54:07 +0300

    add abaout page

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
-from .views import MainView
+from .views import MainView, AboutView
 from django.urls import path
 
 urlpatterns = [
     path('', MainView.as_view(), name="home"),
+    path('about', AboutView.as_view(), name="about")
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-# Create your views here.
+
 
 class MainView(TemplateView):
     template_name = "main/index.html"
+
+
+class AboutView(TemplateView):
+    template_name = "main/about.html"
