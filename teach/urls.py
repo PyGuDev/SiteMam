cb333a70df322f766ff8814040ca1904e33f5f57
PyGuDev <gubaev1999@gmail.com>
2020-01-26 21:47:29 +0300

    optimize

--- teach/urls.py
+++ teach/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TeachPage, download
+from .views import TeachPage, download, SearchTeach
 
 urlpatterns = [
     path('', TeachPage.as_view(), name="teach"),
-    path('file/<pk>', download, name="downloadfile")
+    path('file/<pk>', download, name="downloadfile"),
+    path('searchfile', SearchTeach.as_view(), name="searchfile"),
 ]
--- teach/views.py
+++ teach/views.py
@@ -1,13 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.views.generic import ListView, DeleteView
 from .models import TeachFile
-# Create your views here.
+
 
 def download(request, pk):
     obj = TeachFile.objects.get(pk=pk)
     return redirect(obj.file.url)
 
+
 class TeachPage(ListView):
     model = TeachFile
     template_name = "teach/index.html"
+
+
+class SearchTeach(ListView):
+    model = TeachFile
+    template_name = "teach/index.html"
+
+    def get_queryset(self):
+        queryset = TeachFile.objects.all()
+        q = self.request.GET.get("q")
+        print(q)
+        if q:
+            return queryset.filter((Q(title__icontains=q) | Q(description__icontains=q)))
+        return queryset
